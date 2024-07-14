0207eecc4d2117ec2c1b68de8f2e758838406d4b
wadidawCEO <jaya.sutedja@gmail.com>
2024-07-14 16:47:14 +0700

    modify main.py [add 1 function]

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     text = "books/frankenstein.txt"
     file_content = get_book_text(text)
     count_words = get_total_words(file_content)
-    print(count_words, "assasa")
+    each_characters = get_total_each_characters(file_content)
 
 def get_book_text(text):
     # read the content in the specified file
@@ -13,5 +13,18 @@ def get_total_words(file_content):
     # count the words in a text
     return len(file_content.split())
 
+def get_total_each_characters(file_content):
+    # change file_content to lowercase and split every strings into list;
+    # double loop the result and adding it in dictionary
+    dic = {}
+    list_words = file_content.lower().split()
+    for i in list_words:
+        for k in i:
+            if k in dic:   
+                dic[k] += 1
+            else:
+                dic[k] = 1
+    return dic
+ 
 if __name__ == "__main__":
     main()
