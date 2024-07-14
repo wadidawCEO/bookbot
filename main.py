17f5a53fb62250f420f1ddd7bd0f2c3d1b25c3f4
wadidawCEO <jaya.sutedja@gmail.com>
2024-07-14 21:22:33 +0700

    modify main.py [completed the main purpose of the project]

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+dic = {}
+
 def main():
     text = "books/frankenstein.txt"
     file_content = get_book_text(text)
     count_words = get_total_words(file_content)
     each_characters = get_total_each_characters(file_content)
+    print(f"--- Begin report of {text} ---")
+    print(f"{count_words} words found in the document\n")
+    for k,v in convert_dictionary_to_list(dic):
+        print(f"The '{k}' character was found '{v}' times")
 
 def get_book_text(text):
     # read the content in the specified file
@@ -15,14 +21,22 @@ def get_total_words(file_content):
 
 def get_total_each_characters(file_content):
     # change file_content to lowercase, then input it in dic with loop
-    dic = {}
+    # it adds only the alphabet string in list_words to dic
     list_words = file_content.lower()
     for i in list_words:
         if i in dic:   
             dic[i] += 1
-        else:
+        elif i.isalpha() == True:
             dic[i] = 1
+        else:
+            pass
     return dic
+
+def convert_dictionary_to_list(dic):
+    # convert dic to list, then reverse sort it based on its value 
+    temp_dic = dic.items()
+    sorted_result = sorted(temp_dic, key=lambda d: d[1], reverse=True)
+    return sorted_result
  
 if __name__ == "__main__":
     main()
