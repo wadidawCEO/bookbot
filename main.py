e034991703d2c210ef29fcdc3625f1cd72d14fc5
wadidawCEO <jaya.sutedja@gmail.com>
2024-07-14 16:15:51 +0700

    modify main.py [add 3 functions]

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-with open("books/frankenstein.txt") as f:
-    file_content = f.read()
+def main():
+    text = "books/frankenstein.txt"
+    file_content = get_book_text(text)
+    count_words = get_total_words(file_content)
+    print(count_words, "assasa")
 
-main()
+def get_book_text(text):
+    # read the content in the specified file
+    with open(text) as f:
+        return f.read()
+
+def get_total_words(file_content):
+    # count the words in a text
+    return len(file_content.split())
+
+if __name__ == "__main__":
+    main()
