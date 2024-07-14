8e06975e6023b4e2e50de666faa323a4a7a9b081
wadidawCEO <jaya.sutedja@gmail.com>
2024-07-14 16:56:40 +0700

    update get_total_each_characters function for better performance

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     file_content = get_book_text(text)
     count_words = get_total_words(file_content)
     each_characters = get_total_each_characters(file_content)
+    print(each_characters)
 
 def get_book_text(text):
     # read the content in the specified file
@@ -17,13 +18,12 @@ def get_total_each_characters(file_content):
     # change file_content to lowercase and split every strings into list;
     # double loop the result and adding it in dictionary
     dic = {}
-    list_words = file_content.lower().split()
+    list_words = file_content.lower()
     for i in list_words:
-        for k in i:
-            if k in dic:   
-                dic[k] += 1
-            else:
-                dic[k] = 1
+        if i in dic:   
+            dic[i] += 1
+        else:
+            dic[i] = 1
     return dic
  
 if __name__ == "__main__":
